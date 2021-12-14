759418551dd701880b5104675158b3b8d50f6de8
khakwani007 <mustafakhakwani600@gmail.com>
2021-12-14 10:01:01 +0500

    Cpmpleted commit

---
+++ ToasterLibrary/src/main/java/com/example/publish/Toaster.java
@@ -0,0 +1,10 @@
+package com.example.publish;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+    public static void toast(Context context,String message){
+        Toast.makeText(context,message, Toast.LENGTH_SHORT).show();
+    }
+}
