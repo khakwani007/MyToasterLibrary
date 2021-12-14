a8c5787a11644af1ddbea300feaf89970332d3ed
khakwani007 <mustafakhakwani600@gmail.com>
2021-12-14 11:14:42 +0500

    Cpmpleted

--- ToasterLibrary/src/main/java/com/example/publish/Toaster.java
+++ ToasterLibrary/src/main/java/com/example/publish/Toaster.java
@@ -6,5 +6,10 @@ import android.widget.Toast;
 public class Toaster {
     public static void toast(Context context,String message){
         Toast.makeText(context,message, Toast.LENGTH_SHORT).show();
+
+    }
+    public static double add(double a,double b){
+        double sum=a+b;
+        return sum ;
     }
 }
